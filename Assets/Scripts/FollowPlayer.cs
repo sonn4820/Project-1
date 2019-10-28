df9ce9bc116d98c109e552365715bcd4af4ddc56
JackFritz <56080401+JackFritz@users.noreply.github.com>
2019-10-28 12:21:56 -0400

    Add files via upload

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 offset;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + 0.1f * offset;
+    }
+}
+
