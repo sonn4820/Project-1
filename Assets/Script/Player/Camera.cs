2f6293397eab5f2daa101deb4d478295d10287fc
Son Nguyen <sonnguyen4820@gmail.com>
2019-10-28 14:44:59 -0400

    Camera Update for Trash and Poop

--- Assets/Script/Player/Camera.cs
+++ Assets/Script/Player/Camera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    public GameObject player;   // public var that refers to the game object we want to follow player/ball
+    private Vector3 offset;   // private var that determines the camera distance from the player/ball
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + 0.2f * offset;
     }
 }
