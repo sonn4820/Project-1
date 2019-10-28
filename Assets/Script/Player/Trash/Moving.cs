8f43ce800f964d83cd6d85b859aa2c328f2ce14c
Son Nguyen <sonnguyen4820@gmail.com>
2019-10-28 12:15:56 -0400

    Trash moving test

---
+++ Assets/Script/Player/Trash/Moving.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Animator animator; // get animator
+    public float speed = 3f; // set up speed
+    public float moveHorizontal; // call new float for horizontal movement
+    public float moveVertical; // call new float for vertical movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        moveHorizontal = Input.GetAxis("Horizontal") * speed; // move hor
+
+        moveVertical = Input.GetAxis("Vertical") * speed; // move ver
+
+        animator.SetFloat("VerSpeed", moveHorizontal); // get animation
+        animator.SetFloat("HorSpeed", moveVertical); // get animation
+
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal, moveVertical); // get rigidbody for player
+
+     
+
+    }
+}
