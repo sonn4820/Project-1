72063d29003a73ec7168aff823f2258b90159ff8
JackFritz <56080401+JackFritz@users.noreply.github.com>
2019-11-05 13:39:00 -0500

    Add files via upload

---
+++ Assets/Script/Title/ReturnToTitle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+    }
+}
