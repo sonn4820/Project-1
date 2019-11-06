362a9d308c9cc73f59490c9171d6232c8f86980d
JackFritz <56080401+JackFritz@users.noreply.github.com>
2019-11-06 13:24:37 -0500

    Add files via upload

---
+++ Assets/Script/Enemy/Lighter/Move2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2 : MonoBehaviour
+{
+    float speed = -5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            speed = speed * -1;
+        }
+    }
+}
---
+++ Assets/Script/Enemy/SprayBottle/Water2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water2 : MonoBehaviour
+{
+    float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+
+
+        if (transform.position.x > 6f)
+        {
+            transform.position = new Vector2(-4.52f, 8.2f);
+        }
+
+    }
+}
