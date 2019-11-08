e624e553c88264ef8b0043c6ebcec8c37189648d
JackFritz <jfritz@quinnipiac.edu>
2019-11-08 17:15:24 -0500

    TrashLevel

---
+++ Assets/Script/Enemy/Broom/Spin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    // Start the ledge spinning as soon as the game starts
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().angularVelocity = 400;
+    }
+
+
+}
+
